bd22bfdc62948fbdca5092a51f239976dfa81e3a
Martinezz187 <Martin.Martinez187@outlook.com>
2024-07-20 22:41:21 -0300

    Se establecieron medidas al models

--- api-persona/app/Models/persona.php
+++ api-persona/app/Models/persona.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class persona extends Model
 {
     use HasFactory;
+
+    protected $table = 'persona';
+
+    protected $fillable = [
+        'id',
+        'nombre',
+        'apellido',
+        'telefono'
+    ];
 }
